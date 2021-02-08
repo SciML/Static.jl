9a00b915ba703520f8452478d4e8c44ab0fa60ad
chriselrod <elrodc@gmail.com>
2021-02-08 13:16:49 -0500

    Update for latest static additions in ArrayInterface.jl

--- src/Static.jl
+++ src/Static.jl
@@ -1,5 +1,18 @@
 module Static
 
+import IfElse
+using Base: @propagate_inbounds, Slice
+
+export static, StaticInt, StaticSymbol, True, False, StaticBool
+
+@static if VERSION >= v"1.7.0-DEV.421"
+    using Base: @aggressive_constprop
+else
+    macro aggressive_constprop(ex)
+        ex
+    end
+end
+
 include("static.jl")
 include("ranges.jl")
 
