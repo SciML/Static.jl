7a99b500a4c8e6a8a884a762d4122fbd6bf45a0c
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2021-10-06 22:09:35 -0400

    use explicit cases rather than `is_static` checking

--- src/comparables.jl
+++ src/comparables.jl
@@ -17,19 +17,15 @@
 =#
 for (alt, op) in [(:(eq), :(==)), (:(ne), :(!=)), (:(le), :(<=)),
                   (:(lt), :(<)),  (:(ge), :(>=)), (:(gt), :(<))]
-    @eval begin
-        $alt(x::X) where {X} = Base.Fix2($alt, x)
-        $alt(x::X, y::Y) where {X,Y} =
-            ifelse(is_static(X) & is_static(Y), static, identity)(Base.$op(x,y))
-     end
+    for (S,T) in ((:StaticBool, :Bool), (:StaticInt, :Int), (:StaticFloat64, :Float64))
+        @eval begin
+            $alt(x::$S{X}, y::$S{Y}) where {X,Y} = static(Base.$op(X,Y))
+            $alt(x::$S{X}, y::$T) where {X} = Base.$op(X,y)
+            $alt(x::$T, y::$S{Y}) where {Y} = Base.$op(x,Y)
+      end
 end
 
-#=
-for f in [:(==), :(!=), :(<), :(≤), :(>), :(≥)]
-    @eval begin
-        @inline Base.$f(::StaticInt{M}, ::StaticInt{N}) where {M,N} = $f(M, N)
-        @inline Base.$f(::StaticInt{M}, x::Int) where {M} = $f(M, x)
-        @inline Base.$f(x::Int, ::StaticInt{M}) where {M} = $f(x, M)
-    end
+for alt in [:eq, :ne, :le, :lt, :ge, :gt]
+    @eval $alt(x::T) where T = Base.Fix2($alt, x)
 end
-=#
+    
