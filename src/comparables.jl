9103aef34f66bcac66fc027b3183f4a73453a161
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2021-10-06 22:27:11 -0400

    cover all cases

--- src/comparables.jl
+++ src/comparables.jl
@@ -19,9 +19,14 @@ for (alt, op) in [(:(eq), :(==)), (:(ne), :(!=)), (:(le), :(<=)),
                   (:(lt), :(<)),  (:(ge), :(>=)), (:(gt), :(<))]
     for (S,T) in ((:StaticBool, :Bool), (:StaticInt, :Int), (:StaticFloat64, :Float64))
         @eval begin
-            $alt(x::$S{X}, y::$S{Y}) where {X,Y} = static(Base.$op(X,Y))
-            $alt(x::$S{X}, y::$T) where {X} = Base.$op(X,y)
-            $alt(x::$T, y::$S{Y}) where {Y} = Base.$op(x,Y)
+            @inline Base.$op(x::$S{X}, y::$S{Y}) where {X,Y} = Base.$op(X,Y)
+            @inline Base.$op(x::$S{X}, y::$T) where {X} = Base.$op(X,y)
+            @inline Base.$op(x::$T, y::$S{Y}) where {Y} = Base.$op(x,Y)
+
+            @inline $alt(x::$S{X}, y::$S{Y}) where {X,Y} = static(Base.$op(X,Y))
+            @inline $alt(x::$S{X}, y::$T) where {X} = Base.$op(X,y)
+            @inline $alt(x::$T, y::$S{Y}) where {Y} = Base.$op(x,Y)
+            @inline $alt(x::$T, y::$T) = Base.$op(x,y)
         end
     end    
 end
