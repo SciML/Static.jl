4d35acc45a57f7738c6938f2d4ffd99ca00a1f08
Oliver Schulz <oschulz@mpp.mpg.de>
2022-12-01 14:59:28 +0100

    STASH

--- test/runtests.jl
+++ test/runtests.jl
@@ -485,10 +485,10 @@ y = 1:10
     @test typeof(fone)(1.0) isa Static.StaticFloat64
 
     @test @inferred(eltype(Static.StaticFloat64(static(1)))) <: Float64
-    @test @inferred(promote_rule(typeof(fone), Int)) <: promote_type(Float64, Int)
-    @test @inferred(promote_rule(typeof(fone), Float64)) <: Float64
-    @test @inferred(promote_rule(typeof(fone), Float32)) <: Float32
-    @test @inferred(promote_rule(typeof(fone), Float16)) <: Float16
+    @test @inferred(promote_rule(typeof(fone), Int)) == Float64
+    @test @inferred(promote_type(typeof(fone), Float64)) == Float64
+    @test @inferred(promote_type(typeof(fone), Float32)) == Float32
+    @test @inferred(promote_type(typeof(fone), Float16)) == Float16
 
     @test @inferred(inv(static(2.0))) === static(inv(2.0)) === inv(static(2))
 
