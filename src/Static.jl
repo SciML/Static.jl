d80b5762555254738b0a9cd3fb66776f4cb15424
Chris Elrod <elrodc@gmail.com>
2023-03-13 18:06:16 -0400

    Add checkindex method to avoid ambiguity

--- src/Static.jl
+++ src/Static.jl
@@ -85,6 +85,10 @@ Base.to_index(x::StaticInt) = known(x)
 function Base.checkindex(::Type{Bool}, inds::AbstractUnitRange, ::StaticNumber{N}) where {N}
     checkindex(Bool, inds, N)
 end
+function checkindex(::Type{Bool}, inds::Base.IdentityUnitRange,
+                    ::StaticFloat64{N}) where {N}
+    checkindex(Bool, inds.indices, N)
+end
 
 ifelse(::True, @nospecialize(x), @nospecialize(y)) = x
 ifelse(::False, @nospecialize(x), @nospecialize(y)) = y
